90946214bcf11074970dd288a9fa67272e1a060b
AlexM <alex.muromtsev@gmail.com>
2022-08-08 00:04:37 +0300

    add hw4

---
+++ Navigation/Checker.swift
@@ -0,0 +1,32 @@
+
+final class Checker {
+    
+    private let login = "Vasily"
+    private let pswd = "StrongPassword"
+    
+    static let shared = Checker()
+    
+    private init() {}
+    
+    func checkPassword (login: String, pswd: String) -> Bool {
+        
+        //        if login == self.login && pswd == self.pswd {
+        //            return true
+        //        } else {
+        //            return false
+        //        }
+        
+        
+        if login.hash == self.login.hash && pswd.hash == self.pswd.hash {
+            return true
+        } else {
+            return false
+        }
+        
+        
+    }
+}
+
+
+
+
